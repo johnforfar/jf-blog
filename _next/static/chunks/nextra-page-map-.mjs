b238c1628f48e5e63eb5e6c637acfade8c81049f
johnforfar <46335740+johnforfar@users.noreply.github.com>
2025-01-09 22:29:44 +0800

    Updates

---
+++ _next/static/chunks/nextra-page-map-.mjs
@@ -0,0 +1,22 @@
+export const pageMap = [{
+  name: "index",
+  route: "/",
+  frontMatter: {
+    "type": "page",
+    "title": "Welcome to Johnny's Blog!",
+    "date": new Date(1704758400000),
+    "description": "Welcome to my blog about code and technology."
+  }
+}, {
+  name: "posts",
+  route: "/posts",
+  children: [{
+    name: "hello-world",
+    route: "/posts/hello-world",
+    frontMatter: {
+      "title": "Hello World",
+      "date": new Date(1736380800000),
+      "description": "My first blog post with Code Wallet integration"
+    }
+  }]
+}];
